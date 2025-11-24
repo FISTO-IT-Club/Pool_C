3e4a2efc3e1815caafb47b387fe809e761dadaf5
maynaou <mohssinaynaou874@gmail.com>
2025-11-24 17:25:34 +0100

    finale day

---
+++ Day_03/print_alphabet.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<unistd.h>
+
+void print_alphabet(void) {
+    for (int i='a';i<='z';i++) {
+        write(1,&i,1);
+    }
+}
+
+int main() {
+    print_alphabet();
+    return 0;
+}
