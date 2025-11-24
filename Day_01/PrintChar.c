f82e19f1dcd0267b08c8b06c8bd113df840784c3
Xssama <ouhsaini2005@gmail.com>
2025-11-24 21:55:35 +0100

    using write func instead of printf

--- Day_01/PrintChar.c
+++ Day_01/PrintChar.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <io.h>
 
 void PrintChar(char a)
 {
-    printf("%c", a);
+    _write(1, &a, 1);
 }
 int main ()
 {
     char char1 = 'A';
-    PrintChar(char1);
+    PrintChar(&char1);
 }
