c39d0f8343335f05f34a62cd59a967617623b22e
HoudaMerzouki <merzoukihouda68@gmail.com>
2025-11-24 19:23:47 +0100

    pool day01

---
+++ Day_01/print_alphabet.c
@@ -0,0 +1,12 @@
+include<stdio.h>
+void print_alphabet(void){
+    char c='a';
+    while (c<='z'){
+        write(1,&c,1);
+        c++;
+    }
+}
+int main(){
+    print_alphabet();
+    return 0;
+}
