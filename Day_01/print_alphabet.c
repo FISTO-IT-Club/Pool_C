fdeaf47e374a4dbf83bda4eca3f902d155bfcd1b
tahirsara50-max <tahirsara50@gmail.com>
2025-11-24 13:11:19 +0100

    hello

---
+++ Day_01/print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<unistd.h>
+
+void print_alphabet(void);
+
+int main(){
+	print_alphabet();
+	return 0; 
+}
+
+void print_alphabet(void){
+	char s ='a';
+   while( s<='z'){
+		write(1,&s,1);
+		printf("\t");
+		s++;
+	}		
+}
