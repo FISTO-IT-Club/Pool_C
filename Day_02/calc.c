e3691f7a8e95fae1ecc578354bb30cdfd10e2a9e
abderrahim-i7 <abderrahimzedgui@gmail.com>
2025-11-25 18:59:53 +0100

    math&input

---
+++ Day_02/calc.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void calc(void){
+int num1,num2;
+char oper;
+ ptintf("sisair num1 :");
+ scanf("%f",&num1);
+ ptintf("\nsisair num2 :");
+ scanf("%f",&num2);
+printf("\nsisaire operation sur les nombre :");
+scanf("%c",&oper);
+}
