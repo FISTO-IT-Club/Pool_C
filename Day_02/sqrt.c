a687fefcfb4510e6fc74215c328b569d12164ac8
ouafae-boop <ouafaeamoumri@gmail.com>
2025-11-25 22:42:14 +0100

    message

--- Day_02/sqrt.c
+++ Day_02/sqrt.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int sqrt_x(int nb) {
- float r ;
- printf("Entrer un nombre: ");
- scanf("%d", &nb);
- r = sqrt_x(nb);
- printf("La racine carree de %d est: %f\n", nb, r);
- return 0;
+ int i;
+ for( i = 0; i <= nb ; i++) {
+     if (i * i == nb) 
+         return i;
+     }
+     return i - 1;
 }
 
 
