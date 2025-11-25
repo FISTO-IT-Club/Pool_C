32f9052878a9b06d6edf82d3ac62cec708738b73
simoumh0-ux <simoumh0@gmail.com>
2025-11-25 13:14:18 +0100

    add fact.c

---
+++ Day_02/fact.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int fact(int nb) {
+	if(nb<0)
+	return 0;
+	if(nb==0)
+	return 1;
+	
+		return nb *fact(nb-1);
+	
+}
+
+int main()
+{
+    int n ;
+    printf("entrer le nembre\n");
+    scanf("%d",&n);
+    printf("Factorial %d" "= %d\n",n, fact(n));
+    return 0;
+}
