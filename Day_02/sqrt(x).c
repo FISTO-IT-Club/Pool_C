abe96319c23497e9d2e743bb02c7cc2881348711
Nizar Elfennani <elfennani2002@gmail.com>
2025-11-25 12:50:04 +0100

    Extract sqrt logic into a separate function

--- Day_02/sqrt(x).c
+++ Day_02/sqrt(x).c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 
-int main (int argc, char *argv[]) {
-    int x;
-    printf("Enter a number: ");
-    scanf("%d", &x);
-    if (x < 0) {
-        printf("Error! Square root of negative numbers is undefined.\n");
+int sqrt_x(int nb) {
+    if (nb < 0) {
+        return -1;
     } else {
         int result = 0;
         int i = 0;
-        while(i * i <= x) {
+        while(i * i <= nb) {
             result = i;
             i++;
         }
-        printf("The floored square root of %d is %d\n", x, result);
+        return result;
     }
+}
+
+int main (int argc, char *argv[]) {
+    int nb;
+    printf("Enter a number: ");
+    scanf("%d", &nb);
+    int result = sqrt_x(nb);
+
+    if (result == -1) {
+        printf("Error! Square root of negative numbers is undefined.\n");
+        return 1;
+    }
+    
+    printf("The floored square root of %d is %d\n", nb, result);
     return 0;
 }
