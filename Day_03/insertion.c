d0c240d14b6f175c30dd8009aadb7b78d8a0aba5
houda16-gif <houdafg0@gmail.com>
2025-11-26 23:59:14 +0100

    hello

---
+++ Day_03/insertion.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main();
+if (n=0;  n=size;n++);
+int if_insert_pos(int arr[], int size, int n) {
+}
---
+++ Day_03/minmax.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main();
+int min ,max,a;
+int min_max(int arr[],  ) {
+    for (i=0;i<max;i++) {
+    max=0
+} 
