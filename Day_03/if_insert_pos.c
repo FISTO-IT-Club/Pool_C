14fbfed74ffd3653737ddd59eaf79eb7aab81e59
Sakinanejjari <sakinanejj@gmail.com>
2025-11-26 23:01:23 +0100

    day3

---
+++ Day_03/if_insert_pos.c
@@ -0,0 +1,12 @@
+int if_insert_pos(int arr[], int size, int n) {
+    int i;
+    for(i=0;i<size;i++){
+        if(arr[i]==n){
+            return i;
+        }
+        else{
+            
+        }
+
+    }
+}
