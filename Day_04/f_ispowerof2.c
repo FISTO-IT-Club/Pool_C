201931109083ae0ef54119c1f69823a4b2f7715c
Ben Said <bensaidm247@gmail.com>
2025-11-27 22:54:00 +0100

    day 4 completed

---
+++ Day_04/f_ispowerof2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int f_ispowerof2(int x) {
+    if(x%2==0){
+    	printf("%d est une puissance de 2",x);
+	}else{
+		printf("%d n'est pas une puissance de 2",x);
+	}
+}
+
+int main(){
+	int x;
+	printf("entrer la valeur de x :");
+	scanf("%d",&x);
+	f_ispowerof2(x);
+}
