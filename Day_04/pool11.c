355adaea6b3a752b15e1427e78060b9a17aefa83
sanaedour5-droid <sanaedou5@gmail.com>
2025-11-27 20:39:28 +0100

    pool10

--- Day_04/pool11.c
+++
@@ -1,8 +0,0 @@
-int sum_digits(int n) {
-    int sum = 0;
-    int i;
-    for (i = 0; i <= n; i++) {
-        sum = sum + i;
-    }
-    return sum;
-}
--- Day_04/pool12.c
+++
@@ -1,6 +0,0 @@
-int f_ispowerof2(int x) {
-    if (x <= 0) {
-        return 0; 
-    }
-    return (x & (x - 1)) == 0;
-}
