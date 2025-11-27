078e6bf39d5048a10353f4ee711cb74e93c41b4e
boubousouha1-collab <boubousouha1@gmail.com>
2025-11-27 22:50:08 +0100

    Day 4 Completed

---
+++ Day_04/f_ispowerof2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int f_ispowerof2(int x) {
+    if (x <= 0)
+        return 0;
+
+    return (x & (x - 1)) == 0;
+}
+
+int main() {
+    int x;
+    printf("Entrez un nombre : ");
+    scanf("%d", &x);
+
+    if (f_ispowerof2(x))
+        printf("%d est une puissance de 2.\n", x);
+    else
+        printf("%d n'est PAS une puissance de 2.\n", x);
+
+    return 0;
+}
+
