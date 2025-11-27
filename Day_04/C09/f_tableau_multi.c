ffaa3d6b6def84987e5d644f5c95a1dedcc1e838
Ayoub El achab <aelachab21@gmail.com>
2025-11-27 18:27:20 +0100

    Day 3 Completed

---
+++ Day_04/C09/f_tableau_multi.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void f_tableau_multi(int n) {
+
+int i ;
+
+  for ( i = 0; i <=10; i++)
+  {
+    printf("%d x %d = %d\n",n,i,n*i);
+  }
+     
+   
+}
+
+
+int main () {
+
+    int num ;
+
+    printf("entres un nombre ");
+    scanf("%d",&num);
+    printf("\n\n");
+    printf("****************le tablraux de multiplication de %d *************",num);printf("\n\n");
+    f_tableau_multi(num);
+
+return 0;
+}
---
+++ Day_04/C11/f_ispowerof2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+int f_ispowerof2(int x) {
+
+
+    if (x <= 0)
+    {
+        return 0;
+    }
+    
+   while ( x % 2 == 0)
+   {
+    x /= 2;
+   }
+   return 1;
+}
+
+int main () {
+    int nbr;
+    printf("entres un nombre : ");
+    scanf("%d",&nbr);
+
+if (f_ispowerof2(nbr))
+  printf("%d est une puissance de 2.\n", nbr);
+else
+  printf("%d N'EST PAS une puissance de 2.\n", nbr);
+ 
+    return 0;
+
+
+}
