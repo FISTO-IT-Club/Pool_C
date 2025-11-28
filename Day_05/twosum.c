c6d6811190c4314787f4c576d2f65e4ea648f1ed
Xssama <ouhsaini2005@gmail.com>
2025-11-28 22:31:47 +0100

    modify twosum.c file

--- Day_05/twosum.c
+++ Day_05/twosum.c
@@ -2,10 +2,10 @@
 
 void two_sum(int arr[], int size, int target) {
 
-    for (int i =0; i < size; i++) {
+    for (int i =0; i < size - 1; i++) {
         for (int j = i+1; j < size; j++) {
             if (arr[j] + arr[i] == target) {
-                printf("[%d, %d] \n", i+1, j+1);
+                printf("[%d, %d] \n", i, j);
             }
         }
     }
