25c101239fe5bbcad552025d0412ddcd68e297a7
Mohamed Lemine Yahoufdhou <medleminy23@gmail.com>
2025-11-28 23:43:36 +0100

    Create reverse_arr.c

---
+++ Day_05/reverse_arr.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void reverse_array(int arr[]) {
+    for (int i = 4; i >= 0; i--) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    
+    int arr[] = {1, 2, 3, 4, 5};
+    
+    reverse_array(arr);
+    
+    return 0;
+}
