ec6332b12d6db1f5f389ae0e1b0b7e8def412633
FADWA384 <Salmiahmed515@gmail.com>
2025-11-28 22:54:13 +0100

    Day 5 COMPLETED

--- Day_05/twosum.C
+++ Day_05/twosum.C
@@ -1,17 +1,31 @@
-#include <stdio.h> 
+#include <stdio.h>
 void two_sum(int arr[], int size, int target) {
-	for (int i=0;i<size;i++){
-        for (int j=i+1;j<size;j++) 
-		{
-            if (arr[i] + arr[j] == target){
-                printf(" vioci les indices [%d, %d]\n",i+1,j+1);
-                return;
+    int i, j;
+    int found = 0;
+    for (i = 0; i < size - 1; i++) {
+        for (j = i + 1; j < size; j++) {
+            if (arr[i] + arr[j] == target) {
+                printf("Indices adding to %d: [%d, %d]\n", target, i + 1, j + 1);
+                found = 1;
             }
         }
     }
-    printf("il n est ya pas 2 elements dont la somme est %d \n",target);
+    if (!found) {
+        printf("il n'exicte pas une somme de 2 nombres = %d.\n", target);
+    }
 }
-int main(){
-    int t[]={2,7,20,17};
-    two_sum(t,4,22);
+int main() {
+    int size, target;
+    printf("Enter le nombre des elements: ");
+    scanf("%d", &size);
+    int arr[size];
+    printf("Enter %d nombres:\n", size);
+    for (int i = 0; i < size; i++) {
+        printf("nombre %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter une valeur: ");
+    scanf("%d", &target);
+    two_sum(arr, size, target);
+    return 0;
 }
