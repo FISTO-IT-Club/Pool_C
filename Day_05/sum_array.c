07ae32571f438c75a428b4e8c6f39a8c2323be51
Mohamed Lemine Yahoufdhou <medleminy23@gmail.com>
2025-11-28 23:42:09 +0100

    Create sum_array.c

---
+++ Day_05/sum_array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int sum_array(int arr[]) {
+    int sum = 0;
+    for (int i = 0; i < 5; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    printf("%d\n", sum_array(arr));
+    return 0;
+}
