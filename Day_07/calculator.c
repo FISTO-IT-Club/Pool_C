28839dc9151c220d4af747e02e180b67eaa0d36b
lina-ramdani <lina.ramdani14@gmail.com>
2025-11-30 23:16:37 +0100

    Day 7 completed

--- Day_07/calculator.c
+++ Day_07/calculator.c
@@ -3,17 +3,28 @@
 float calculator(float a, float b) {
     char op, reponse ;
     float result ;
+    int i, f1=1, f2=1 ;
 
     do{
-        printf("saisir une opération (+, -, *, /):") ;
+        printf("saisir une opération (+, -, *, /, pwr(2), pwr(3), fact):") ;
         scanf(" %c", &op) ;
+
         switch(op){
                 case '+': result=a+b; break;
                 case '-': result=a-b; break;
                 case '*': result=a*b; break;
+
                 case '/': if(b==0) {printf("impossible de diviser par 0") ; continue ;}
-                         else result=a/b; break;
+                          else result=a/b; break;
+
+                case 'pwr(2)': result=a*a; result=b*b; break;
+                case 'pwr(3)': result=a*a*a; result=b*b*b; break;
+
+                case 'fact': for(i=1; i<=a; i++){f1=f1*i;} result=f1;
+                             for(i=1; i<=b; i++){f2=f2*i;} result=f2;
+                             break;
                 } 
+
         printf("le resultat est: %f", result) ;
 
         printf("veut-tu faire un autre calcul? tapez O si oui, tapez N si non:") ;
